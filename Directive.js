9b329314949836eb511628fac5134aae2b281f35
David_Yu <yuyang545262477@gmail.com>
2016-06-28 12:15:04 +0800

    directive get datas form scope .

--- Directive.js
+++ Directive.js
@@ -5,9 +5,8 @@
     .controller('myCtrl', myCtrl)
     .directive('unorderedList', unorderedList);
     
-    function myCtrl() {
-        var vm = this;
-        vm.products = [
+    function myCtrl($scope) {
+        $scope.products = [
             {name: "Apples", category: "Fruit", price: 1.20, expiry: 10},
             {name: "Bananas", category: "Fruit", price: 2.42, expiry: 7},
             {name: "Pears", category: "Fruit", price: 2.02, expiry: 6}];
@@ -15,7 +14,14 @@
     
     function unorderedList() {
         return function (scope, element, attrs) {
-            
+            var data = scope[attrs["unorderedList"]];
+            if (angular.isArray(data)) {
+                for (var i = 0; i < data.length; i++) {
+                    console.log(data[i].name);
+                }
+            } else {
+                console.log("not a array");
+            }
         }
         
     }
